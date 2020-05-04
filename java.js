9f66b31ee874d8175db65560c5ae63a4a4001b86
New Name <mubin.momin@nxp.com>
2020-05-04 11:04:49 -0500

    array of alphabet added

--- java.js
+++ java.js
@@ -11,6 +11,7 @@ character = ["!", "@", "#", "$", "&", "%", " ^ ", "*", "(", ")", "+", "-", ".", 
 // create array for numbers
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // create array for letters
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // create a variable that change lower case to upper case letters for password
 
